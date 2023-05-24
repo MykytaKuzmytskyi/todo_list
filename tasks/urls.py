c1e8b90d686a34471e4d3723a3de6915a828bf20
Mykyta Kuzmytskyi <nykytakuzmytskyi@gmail.com>
2023-05-24 21:44:35 +0300

    Added functions for task model.

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
 
-from .views import TaskListView
-
+from .views import (
+    TaskListView,
+    TaskCreateView,
+    TaskUpdateView,
+    TaskDeleteView,
+    closing_task
+)
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="task-list"),
+    path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
+    path("tasks/update/<int:pk>/", TaskUpdateView.as_view(), name="task-update"),
+    path("tasks/delete/<int:pk>/", TaskDeleteView.as_view(), name="task-delete"),
+    path("tasks/<int:pk>/close/", closing_task, name="task-close"),
 ]
 
 app_name = "tasks"
--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,39 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
+from tasks.forms import TaskForm
 from tasks.models import Task
 
 
 class TaskListView(generic.ListView):
     model = Task
     template_name = "tasks/task_list.html"
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("tasks:task-list")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("tasks:task-list")
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("tasks:task-list")
+
+
+def closing_task(request, pk):
+    task = Task.objects.get(id=pk)
+    if task.is_completed is False:
+        task.is_completed = True
+        task.save()
+    return HttpResponseRedirect(
+        reverse_lazy("tasks:task-list")
+    )
