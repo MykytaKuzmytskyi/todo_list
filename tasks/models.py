8d71a8c97487558b75006209ea5868d7a0060a54
Mykyta Kuzmytskyi <nykytakuzmytskyi@gmail.com>
2023-05-24 21:25:23 +0300

    Created models

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    description = models.TextField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateField()
+    is_completed = models.BooleanField(default=False)
+    tags = models.ManyToManyField("Tag", related_name="tasks")
+
+    class Meta:
+        ordering = ["is_completed", "-creation_date"]
