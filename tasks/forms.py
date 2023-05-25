d12afca78560d53c3a723744b013296f31ced89d
Mykyta Kuzmytskyi <nykytakuzmytskyi@gmail.com>
2023-05-26 01:22:47 +0300

    Added validate for deadline field.

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,4 +1,7 @@
+import datetime
+
 from django import forms
+from django.core.exceptions import ValidationError
 
 from tasks.models import Task, Tag
 
@@ -9,6 +12,14 @@ class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ("description", "tags", "deadline")
+
+    def clean_deadline(self):
+        deadline = self.cleaned_data["deadline"]
+        if deadline < datetime.date.today():
+            raise ValidationError(
+                "The date cannot be in the past!"
+            )
+        return deadline
 
 
 class TagForm(forms.ModelForm):
